89d1b43eefbb3e16d4958b99ccb385d1b609ca12
tamim-devs <coderhero1@gmail.com>
2026-08-18 00:15:03 +0600

    deploy fix

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("medique");
     const tutorCollection = db.collection("tutor");
